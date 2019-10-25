dd0a78a30c60c04484ec87b0844e5a23faf9a9e4
wilantury <ventas@antury.net>
2019-10-25 07:57:49 -0500

    CRUD mongoDb

--- services/movies.js
+++ services/movies.js
@@ -1,29 +1,36 @@
 //capa de servicios: se comunican con otros servicios y con librerías. El controlador solo se comunica con servicios
-const { moviesMock } = require('../utils/mocks/movies.js');
+//const { moviesMock } = require('../utils/mocks/movies.js')
+//ahora usaremos nuestra base de datos de mongodb
+const MongoLib = require('../lib/mongo');
 
 class MovieService{
-  async getMovies(){
-    const movies = await Promise.resolve(moviesMock);
+  constructor(){
+    this.collection = 'movies';
+    this.mongoDb =  new MongoLib();
+  }
+  async getMovies( { tags }){
+    const query = tags && {tags: { $in: tags}}//sintaxis de mongo
+    const movies = await this.mongoDb.getAll(this.collection, query);//Promise.resolve(moviesMock);
     return movies || [];
   }
 
-  async getMovie(){
-    const movie = await Promise.resolve(moviesMock[0])
+  async getMovie({ movieId }){
+    const movie = await this.mongoDb.get(this.collection, movieId);//Promise.resolve(moviesMock[0])
     return movie || {};
   }
 
-  async createMovie(){
-    const movieCreatedId = await Promise.resolve(moviesMock[0].id);
+  async createMovie({ movie }){
+    const movieCreatedId = await this.mongoDb.create(this.collection, movie);//Promise.resolve(moviesMock[0].id);
     return movieCreatedId;
   }
 
-  async updateMovie(){
-    const movieUpdatedId = await Promise.resolve(moviesMock[0].id);
+  async updateMovie({ movieId, movie } = {}){
+    const movieUpdatedId = await this.mongoDb.update(this.collection, movieId, movie);//Promise.resolve(moviesMock[0].id);
     return movieUpdatedId;
   }
 
-  async deleteMovie(){
-    const movieDeletedId = await Promise.resolve(moviesMock[0].id);
+  async deleteMovie({ movieId }){
+    const movieDeletedId = this.mongoDb.delete(this.collection, movieId);//await Promise.resolve(moviesMock[0].id);
     return movieDeletedId;
   }
 }
