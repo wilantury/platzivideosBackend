07b18942d601771f488df98a03128e397ed21ae6
wilantury <ventas@antury.net>
2019-10-25 16:11:36 -0500

    CRUD mongo-Connection Done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
+//middleware Body parser
+app.use(express.json());
 
 moviesApi(app);
 //rutas
